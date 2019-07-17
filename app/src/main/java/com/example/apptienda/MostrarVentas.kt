756881ff8cac50df37be7c284699c2643bb17ac5
jes22 <albert123ord@gmail.com>
2019-07-17 10:33:02 -0600

    Segundo Commit

---
+++ app/src/main/java/com/example/apptienda/MostrarVentas.kt
@@ -0,0 +1,12 @@
+package com.example.apptienda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MostrarVentas : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostrar_ventas)
+    }
+}
